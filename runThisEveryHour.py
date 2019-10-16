a95da0592e873e13c7220cad96cacff9fe186089
JEndler <jakob.endler@hotmail.de>
2019-10-16 13:39:01 +0200

    Reorganised File Structure and Database

--- runThisEveryHour.py
+++
@@ -1,6 +0,0 @@
-import OddsScraper
-
-link_list = OddsScraper.findMatchLinks(OddsScraper.getRawData("https://www.hltv.org/matches"))
-
-for link in link_list:
-	OddsScraper.analyseUpcomingMatch(link)
--- runThisEveryday.py
+++
@@ -1,4 +0,0 @@
-import HLTVScraper
-
-HLTVScraper.update_data()
-HLTVScraper.getPredictions(days = 1, writeToFile = True)

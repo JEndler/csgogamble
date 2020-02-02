310a2d5be228a5a7ab55c37c76a1ad7d23814715
JEndler <jakob.endler@hotmail.de>
2020-02-02 21:35:43 +0100

    fixed small datetime bug

--- alert.py
+++ alert.py
@@ -4,7 +4,7 @@ import datetime
 
 def hours_since_modification(filename = "/home/projects/csgogamble/data/odds.txt"):
   t = os.path.getmtime(filename)
-  return (datetime.now() - datetime.datetime.fromtimestamp(t)).total_seconds()/60/60
+  return (datetime.datetime.now() - datetime.datetime.fromtimestamp(t)).total_seconds()/60/60
 
 if hours_since_modification() > 24:
 	client = Client("AC66c0f572c83b77d8b78e6832e545c2ab", "4f7dec8ae2b64d23f959a23ae22399c7")
